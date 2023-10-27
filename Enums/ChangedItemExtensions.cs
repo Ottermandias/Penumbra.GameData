e1a62d8e6b4e1d8c482253ad14850fd3dc372d86
Ottermandias <nickroederspam+git1@gmail.com>
2023-10-28 01:12:47 +0200

    Handle ItemOffhand.

--- Enums/ChangedItemExtensions.cs
+++ Enums/ChangedItemExtensions.cs
@@ -1,4 +1,3 @@
-using Dalamud.Plugin.Services;
 using Lumina.Excel.GeneratedSheets;
 using Penumbra.Api.Enums;
 using Action = Lumina.Excel.GeneratedSheets.Action;
@@ -11,22 +10,10 @@ public static class ChangedItemExtensions
     {
         return item switch
         {
-            null     => (ChangedItemType.None, 0),
-            Item i   => (ChangedItemType.Item, i.RowId),
-            Action a => (ChangedItemType.Action, a.RowId),
-            _        => (ChangedItemType.Customization, 0),
-        };
-    }
-
-    public static object? GetObject(this ChangedItemType type, IDataManager manager, uint id)
-    {
-        return type switch
-        {
-            ChangedItemType.None          => null,
-            ChangedItemType.Item          => manager.GetExcelSheet<Item>()?.GetRow(id),
-            ChangedItemType.Action        => manager.GetExcelSheet<Action>()?.GetRow(id),
-            ChangedItemType.Customization => null,
-            _                             => throw new ArgumentOutOfRangeException(nameof(type), type, null),
+            null                      => (ChangedItemType.None, 0),
+            (Item i, FullEquipType t) => (t.IsOffhandType() ? ChangedItemType.ItemOffhand : ChangedItemType.Item, i.RowId),
+            Action a                  => (ChangedItemType.Action, a.RowId),
+            _                         => (ChangedItemType.Customization, 0),
         };
     }
 }
