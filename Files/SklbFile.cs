b6a68ab60be6a46f8ede63425cd0716dedf693a3
ackwell <saxon@ackwell.com.au>
2023-12-30 02:40:41 +1100

    Add rudimentary SklbFile

---
+++ Files/SklbFile.cs
@@ -0,0 +1,55 @@
+using Lumina.Extensions;
+
+namespace Penumbra.GameData.Files;
+
+public class SklbFile
+{
+    private const int SklbMagic = 0x736B6C62; // "sklb"
+
+    public SklbVersion Version;
+    public byte[] Skeleton;
+
+    public SklbFile(byte[] data)
+    {
+        using var stream = new MemoryStream(data);
+        using var reader = new BinaryReader(stream);
+
+        var magic = reader.ReadUInt32();
+        if (magic != SklbMagic)
+            throw new InvalidDataException("Invalid sklb magic");
+
+        Version = (SklbVersion)reader.ReadUInt32();
+  
+        var oldHeader = Version switch {
+            SklbVersion.V1100 or SklbVersion.V1110 or SklbVersion.V1200 => true,
+            SklbVersion.V1300 => false,
+            _ => throw new InvalidDataException($"Unknown version {Version}")
+        };
+
+        // Skeleton offset directly follows the layer offset.
+        // TODO: Flesh out further fields of the file.
+        uint skeletonOffset;
+        if (oldHeader)
+        {
+            reader.ReadInt16();
+            skeletonOffset = reader.ReadUInt16();
+        }
+        else
+        {
+            reader.ReadUInt32();
+            skeletonOffset = reader.ReadUInt32();
+        }
+
+        reader.Seek(skeletonOffset);
+        Skeleton = reader.ReadBytes((int)(reader.BaseStream.Length - skeletonOffset));
+    }
+
+    /// <summary> Version of a .sklb file. This is representative of the version specified in the SE-specific header, not the contained havok data. </summary>
+    public enum SklbVersion
+    {
+        V1100 = 0x31313030, // "1100"
+        V1110 = 0x31313130, // "1110"
+        V1200 = 0x31323030, // "1200"
+        V1300 = 0x31333030, // "1300"
+    }
+}
