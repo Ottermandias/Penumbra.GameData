0c3c0227f2524b9868d3c72956b5e5d3e47ea9bc
Ottermandias <nickroederspam+git1@gmail.com>
2024-07-07 16:13:04 +0200

    Update Screen actor indices.

--- Structs/IdTypes.cs
+++ Structs/IdTypes.cs
@@ -519,7 +519,7 @@ public readonly record struct ObjectIndex(ushort Index) : IComparisonOperators<O
     public static readonly ObjectIndex Card6           = new((ushort)ScreenActor.Card6);
     public static readonly ObjectIndex Card7           = new((ushort)ScreenActor.Card7);
     public static readonly ObjectIndex Card8           = new((ushort)ScreenActor.Card8);
-    public static readonly ObjectIndex IslandStart     = new(529);
+    public static readonly ObjectIndex IslandStart     = new(539);
 
     public static implicit operator ObjectIndex(ushort index)
         => new(index);
