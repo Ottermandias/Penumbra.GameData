6e727c86c4ff04c50662b4c3ad0fffb06348e704
Exter-N <exter-n@exter-n.fr>
2024-08-03 15:18:27 +0200

    Add memory-mapped I/O `Memory<byte>` facility

---
+++ Files/Utility/MmioMemoryManager.cs
@@ -0,0 +1,65 @@
+using System.Buffers;
+using System.IO.MemoryMappedFiles;
+
+namespace Penumbra.GameData.Files.Utility;
+
+/// <summary> Allows wrapping a memory-mapped file in a <see cref="Memory{byte}"/>. </summary>
+/// <param name="accessor"> A view accessor over the file's memory. </param>
+/// <param name="file"> The file, if it shall be closed on disposal, otherwise <c>null</c>. </param>
+/// <param name="leaveOpen"> Whether to leave <paramref name="accessor"/> open on disposal. </param>
+public sealed unsafe class MmioMemoryManager(MemoryMappedViewAccessor accessor, MemoryMappedFile? file = null, bool leaveOpen = false) : MemoryManager<byte>
+{
+    private readonly MemoryMappedViewAccessor _accessor  = accessor;
+    private readonly MemoryMappedFile?        _file      = file;
+    private readonly bool                     _leaveOpen = leaveOpen;
+
+    private byte* _pointer;
+
+    public override Memory<byte> Memory
+        => CreateMemory(Length);
+
+    public bool CanRead
+        => _accessor.CanRead;
+
+    public bool CanWrite
+        => _accessor.CanWrite;
+
+    public int Length
+        => (int)_accessor.Capacity;
+
+    public MmioMemoryManager(MemoryMappedFile file, bool leaveOpen = false)
+        : this(file.CreateViewAccessor(), leaveOpen ? null : file)
+    { }
+
+    /// <seealso cref="MemoryMappedFile.CreateFromFile(string, FileMode, string?, long, MemoryMappedFileAccess)"/>
+    public static MmioMemoryManager CreateFromFile(string path, FileMode mode = FileMode.Open, string? mapName = null, long capacity = 0, MemoryMappedFileAccess access = MemoryMappedFileAccess.ReadWrite)
+        => new(MemoryMappedFile.CreateFromFile(path, mode, mapName, capacity, access));
+
+    public override Span<byte> GetSpan()
+        => new(Pin(0).Pointer, Length);
+
+    public override MemoryHandle Pin(int elementIndex = 0)
+    {
+        if (_pointer == null)
+            _accessor.SafeMemoryMappedViewHandle.AcquirePointer(ref _pointer);
+        return new MemoryHandle(_pointer + elementIndex, pinnable: this);
+    }
+
+    public override void Unpin()
+    { }
+
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            if (_pointer != null)
+            {
+                _pointer = null;
+                _accessor.SafeMemoryMappedViewHandle.ReleasePointer();
+            }
+            if (!_leaveOpen)
+                _accessor.Dispose();
+            _file?.Dispose();
+        }
+    }
+}
