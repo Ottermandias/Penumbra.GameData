636188d103ae162f102e5275b5e9bb3383bbeca4
Haselnussbomber <mail@haselnussbomber.de>
2025-04-07 22:15:56 +0200

    Proper NPC/Companion names

--- DataContainers/DictBNpc.cs
+++ DataContainers/DictBNpc.cs
@@ -1,25 +1,27 @@
 using System.Collections.Frozen;
+using Dalamud.Game.ClientState.Objects.Enums;
 using Dalamud.Plugin;
 using Dalamud.Plugin.Services;
 using Lumina.Excel.Sheets;
 using OtterGui.Log;
-using Penumbra.GameData.Data;
 using Penumbra.GameData.DataContainers.Bases;
 using Penumbra.GameData.Structs;
 
 namespace Penumbra.GameData.DataContainers;
 
+#pragma warning disable SeStringEvaluator
+
 /// <summary> A dictionary that matches BNpcNameId to names. </summary>
-public sealed class DictBNpc(IDalamudPluginInterface pluginInterface, Logger log, IDataManager gameData)
-    : NameDictionary(pluginInterface, log, gameData, "BNpcs", Version.DictBNpc, () => CreateBNpcData(gameData))
+public sealed class DictBNpc(IDalamudPluginInterface pluginInterface, Logger log, IDataManager gameData, ISeStringEvaluator evaluator)
+    : NameDictionary(pluginInterface, log, gameData, "BNpcs", Version.DictBNpc, () => CreateBNpcData(gameData, evaluator))
 {
     /// <summary> Create the data. </summary>
-    private static IReadOnlyDictionary<uint, string> CreateBNpcData(IDataManager gameData)
+    private static IReadOnlyDictionary<uint, string> CreateBNpcData(IDataManager gameData, ISeStringEvaluator evaluator)
     {
         var sheet = gameData.GetExcelSheet<BNpcName>(gameData.Language)!;
-        var dict  = new Dictionary<uint, string>((int) sheet.Count);
+        var dict = new Dictionary<uint, string>(sheet.Count);
         foreach (var n in sheet.Where(n => n.Singular.ByteLength > 0))
-            dict.TryAdd(n.RowId, DataUtility.ToTitleCaseExtended(n.Singular, n.Article));
+            dict.TryAdd(n.RowId, evaluator.EvaluateObjStr(ObjectKind.BattleNpc, n.RowId, gameData.Language));
         return dict.ToFrozenDictionary();
     }
 
